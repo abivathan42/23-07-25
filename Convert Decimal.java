1f4f56d01a48c81af821e01493e180f8c5c7f650
abivathan42 <abivathan48@gmail.com>
2025-06-23 21:44:34 +0530

    Create Convert Decimal.java

---
+++ Convert Decimal.java
@@ -0,0 +1,7 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int decimal = 25;
+        String binary = Integer.toBinaryString(decimal);
+        System.out.println("Binary: " + binary);
+    }
+}
